215d4d76511fcf79b5594557c04fb9924d1dd00e
Sivakarthik-programmer <sivakarthik385@gmail.com>
2026-03-15 11:25:23 +0530

    updated cors for netlify

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://your-actual-netlify-url.netlify.app'
+}))
 
 const tmdb = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
